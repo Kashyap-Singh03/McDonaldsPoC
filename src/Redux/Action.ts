73e5158b7caa7a5892f6dd81cad4183dd84a4fec
Kashyap-Singh03 <kashyap.a.singh@capgemini.com>
2023-09-28 13:22:36 +0530

    redux implementation

--- src/Redux/Action.ts
+++ src/Redux/Action.ts
@@ -1,58 +1,107 @@
+export const SIGNUP_REQUEST = 'SIGNUP_REQUEST';
+export const SIGNUP_SUCCESS = 'SIGNUP_SUCCESS';
+export const SIGNUP_FAILURE = 'SIGNUP_FAILURE';
+export const LOGIN_REQUEST = 'LOGIN_REQUEST';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
+export const LOGOUT_REQUEST = 'LOGOUT_REQUEST';
+export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
+export const LOGOUT_FAILURE = 'LOGOUT_FAILURE';
+export const ADD_TO_CART_REQUEST = 'ADD_TO_CART_REQUEST';
+export const ADD_TO_CART_SUCCESS = 'ADD_TO_CART_SUCCESS';
+export const ADD_TO_CART_FAILURE = 'ADD_TO_CART_FAILURE';
+export const REMOVE_FROM_CART_REQUEST = 'REMOVE_FROM_CART_REQUEST';
+export const REMOVE_FROM_CART_SUCCESS = 'REMOVE_FROM_CART_SUCCESS';
+export const REMOVE_FROM_CART_FAILURE = 'REMOVE_FROM_CART_FAILURE';
+export const DELETE_CART_ITEM_REQUEST = 'DELETE_CART_ITEM_REQUEST';
+export const DELETE_CART_ITEM_SUCCESS = 'DELETE_CART_ITEM_SUCCESS';
+export const DELETE_CART_ITEM_FAILURE = 'DELETE_CART_ITEM_FAILURE';
+
 export const signup_request = (data: any) => ({
-  type: 'SIGNUP_REQUEST',
+  type: SIGNUP_REQUEST,
   payload: data,
 });
 
 export const signup_success = (data: any) => ({
-  type: 'SIGNUP_SUCCESS',
+  type: SIGNUP_SUCCESS,
   payload: data,
 });
 
+export const signup_failure = (error: any) => ({
+  type: SIGNUP_FAILURE,
+  payload: error,
+});
+
 export const login_request = (data: any) => ({
-  type: 'LOGIN_REQUEST',
+  type: LOGIN_REQUEST,
   payload: data,
 });
 
 export const login_success = (data: any) => ({
-  type: 'LOGIN_SUCCESS',
+  type: LOGIN_SUCCESS,
   payload: data,
+});
+
+export const login_failure = (error: any) => ({
+  type: LOGIN_FAILURE,
+  payload: error,
 });
 
 export const logout_request = () => ({
-  type: 'LOGOUT_REQUEST',
+  type: LOGOUT_REQUEST,
 });
 
 export const logout_success = (data: any) => ({
-  type: 'LOGOUT_SUCCESS',
+  type: LOGOUT_SUCCESS,
   payload: data,
 });
 
+export const logout_failure = (error: any) => ({
+  type: LOGOUT_FAILURE,
+  payload: error,
+});
+
 export const add_to_cart_request = (userId: any, item: any) => ({
-  type: 'ADD_TO_CART_REQUEST',
+  type: ADD_TO_CART_REQUEST,
   payload: {userId, item},
 });
 
 export const add_to_cart_success = (userId: any, item: any) => ({
-  type: 'ADD_TO_CART_SUCCESS',
+  type: ADD_TO_CART_SUCCESS,
   payload: {userId, item},
 });
 
+export const add_to_cart_failure = (error: any) => ({
+  type: ADD_TO_CART_FAILURE,
+  payload: error,
+});
+
 export const remove_from_cart_request = (userId: any, item: any) => ({
-  type: 'REMOVE_FROM_CART_REQUEST',
+  type: REMOVE_FROM_CART_REQUEST,
   payload: {userId, item},
 });
 
 export const remove_from_cart_success = (userId: any, item: any) => ({
-  type: 'REMOVE_FROM_CART_SUCCESS',
+  type: REMOVE_FROM_CART_SUCCESS,
   payload: {userId, item},
 });
 
+export const remove_from_cart_failure = (error: any) => ({
+  type: REMOVE_FROM_CART_FAILURE,
+  payload: error,
+});
+
 export const delete_cart_item_request = (userId: any, item: any) => ({
-  type: 'DELETE_CART_ITEM_REQUEST',
+  type: DELETE_CART_ITEM_REQUEST,
   payload: {userId, item},
 });
 
 export const delete_cart_item_success = (userId: any, item: any) => ({
-  type: 'DELETE_CART_ITEM_SUCCESS',
+  type: DELETE_CART_ITEM_SUCCESS,
   payload: {userId, item},
+});
+
+export const delete_cart_item_failure = (error: any) => ({
+  type: DELETE_CART_ITEM_FAILURE,
+  payload: error,
 });
--- src/Redux/Reducer.ts
+++ src/Redux/Reducer.ts
@@ -1,5 +1,25 @@
 //@ts-nocheck
 import {combineReducers} from 'redux';
+import {
+  ADD_TO_CART_FAILURE,
+  ADD_TO_CART_REQUEST,
+  ADD_TO_CART_SUCCESS,
+  DELETE_CART_ITEM_FAILURE,
+  DELETE_CART_ITEM_REQUEST,
+  DELETE_CART_ITEM_SUCCESS,
+  LOGIN_FAILURE,
+  LOGIN_REQUEST,
+  LOGIN_SUCCESS,
+  LOGOUT_FAILURE,
+  LOGOUT_REQUEST,
+  LOGOUT_SUCCESS,
+  REMOVE_FROM_CART_FAILURE,
+  REMOVE_FROM_CART_REQUEST,
+  REMOVE_FROM_CART_SUCCESS,
+  SIGNUP_FAILURE,
+  SIGNUP_REQUEST,
+  SIGNUP_SUCCESS,
+} from './Action';
 
 const initialState = {
   loading: false,
@@ -7,52 +27,73 @@ const initialState = {
   isLogin: false,
   currentUser: null,
   cartItems: [],
+  error: '',
 };
 
 const reducer = (state = initialState, action: any) => {
   switch (action.type) {
-    case 'SIGNUP_REQUEST':
+    case SIGNUP_REQUEST:
       return {
         ...state,
         loading: true,
       };
-    case 'SIGNUP_SUCCESS':
+    case SIGNUP_SUCCESS:
       return {
         ...state,
         users: [...state.users, action.payload],
         loading: false,
       };
-    case 'LOGIN_REQUEST':
+    case SIGNUP_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.error,
+      };
+    case LOGIN_REQUEST:
       return {
         ...state,
         loading: true,
         isLogin: false,
       };
-    case 'LOGIN_SUCCESS':
+    case LOGIN_SUCCESS:
       return {
         ...state,
         loading: false,
         currentUser: action.payload,
         isLogin: true,
       };
-    case 'LOGOUT_REQUEST':
+    case LOGIN_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        isLogin: false,
+        error: action.error,
+      };
+    case LOGOUT_REQUEST:
       return {
         ...state,
         loading: true,
       };
-    case 'LOGOUT_SUCCESS':
+    case LOGOUT_SUCCESS:
       return {
         ...state,
         loading: false,
         isLogin: action.payload,
         currentUser: null,
       };
-    case 'ADD_TO_CART_REQUEST':
+    case LOGOUT_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        isLogin: null,
+        error: action.error,
+      };
+    case ADD_TO_CART_REQUEST:
       return {
         ...state,
         loading: true,
       };
-    case 'ADD_TO_CART_SUCCESS':
+    case ADD_TO_CART_SUCCESS:
       let myindex = -1;
       state.cartItems.map((item, index) => {
         if (
@@ -80,12 +121,18 @@ const reducer = (state = initialState, action: any) => {
         loading: false,
         cartItems: [...state.cartItems],
       };
-    case 'REMOVE_FROM_CART_REQUEST':
+    case ADD_TO_CART_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.error,
+      };
+    case REMOVE_FROM_CART_REQUEST:
       return {
         ...state,
         loading: true,
       };
-    case 'REMOVE_FROM_CART_SUCCESS':
+    case REMOVE_FROM_CART_SUCCESS:
       let startingIndex = -1;
       state.cartItems.map((item, index) => {
         if (
@@ -105,12 +152,18 @@ const reducer = (state = initialState, action: any) => {
         loading: false,
         cartItems: [...state.cartItems],
       };
-    case 'DELETE_CART_ITEM_REQUEST':
+    case REMOVE_FROM_CART_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.error,
+      };
+    case DELETE_CART_ITEM_REQUEST:
       return {
         ...state,
         loading: true,
       };
-    case 'DELETE_CART_ITEM_SUCCESS':
+    case DELETE_CART_ITEM_SUCCESS:
       const updatedCartItems = state.cartItems.filter(
         item =>
           item.id !== action.payload.item.id &&
@@ -119,6 +172,12 @@ const reducer = (state = initialState, action: any) => {
       return {
         ...state,
         cartItems: updatedCartItems,
+      };
+    case DELETE_CART_ITEM_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.error,
       };
     default:
       return state;
